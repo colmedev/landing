d43d79f2cd80a9b1e3f1856b4d713d68527e6ede
colmedev <m.colmenarez.dev@gmail.com>
2024-10-27 13:41:12 -0400

    feat: :tada: the begining of the project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Test para el NextUi</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
